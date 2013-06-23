9c0c1fb018327a77818b338a3053602b84d35c8b
markevich <slava.markevich@gmail.com>
2013-06-23 13:47:17 +0300

    Add  tests

--- spec/delivery_method_spec.rb
+++ spec/delivery_method_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 describe DeliveryMethod do
   describe '.new' do
-    context 'Without options' do
+    context 'With incorrect options' do
       it { expect{ DeliveryMethod.new }.to raise_error DeliveryMethod::InvalidOptions}
       it { expect{ DeliveryMethod.new(email: 'test@test.com') }.to raise_error DeliveryMethod::InvalidOptions }
       it { expect{ DeliveryMethod.new(api_key: 'api_secret') }.to raise_error DeliveryMethod::InvalidOptions }
     end
-    it { expect{ DeliveryMethod.new(api_key: 'api_secret', email: 'test@test.com') }.to_not raise_error }
+    context 'With correct options' do
+      it { expect{ DeliveryMethod.new(api_key: 'api_secret', email: 'test@test.com') }.to_not raise_error }
+    end
   end
 end
