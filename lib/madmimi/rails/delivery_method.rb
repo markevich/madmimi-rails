4fa48104e596592af94337c64c871fe6c30e3115
markevich <slava.markevich@gmail.com>
2013-06-23 14:02:24 +0300

    add test for set settings

--- lib/madmimi/rails/delivery_method.rb
+++ lib/madmimi/rails/delivery_method.rb
@@ -1,7 +1,10 @@
 class Madmimi::Rails::DeliveryMethod
   class InvalidOptions < StandardError; end;
 
+  attr_accessor :settings
+
   def initialize config = {}
     raise(InvalidOptions, "Missing configuration") if config[:email].nil? || config[:api_key].nil?
+    self.settings = config
   end
 end
--- spec/delivery_method_spec.rb
+++ spec/delivery_method_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 describe DeliveryMethod do
+  let(:valid_params) { {api_key: 'api_secret', email: 'test@test.com'} }
   describe '.new' do
     context 'With incorrect options' do
       it { expect{ DeliveryMethod.new }.to raise_error DeliveryMethod::InvalidOptions}
@@ -7,7 +8,11 @@ describe DeliveryMethod do
       it { expect{ DeliveryMethod.new(api_key: 'api_secret') }.to raise_error DeliveryMethod::InvalidOptions }
     end
     context 'With correct options' do
-      it { expect{ DeliveryMethod.new(api_key: 'api_secret', email: 'test@test.com') }.to_not raise_error }
+      it { expect{ DeliveryMethod.new(valid_params) }.to_not raise_error }
+    end
+    it 'Set settings' do
+      instance = DeliveryMethod.new(valid_params)
+      expect(instance.settings).to be_equal valid_params
     end
   end
 end
